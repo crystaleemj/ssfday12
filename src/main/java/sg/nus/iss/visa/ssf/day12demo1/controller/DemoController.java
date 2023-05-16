ceadfd4a534368ba549aa943ee23abde55668a59
crystaleemj <ceemj_@hotmail.com>
2023-05-16 10:25:04 +0800

    add comments

--- src/main/java/sg/nus/iss/visa/ssf/day12demo1/controller/DemoController.java
+++ src/main/java/sg/nus/iss/visa/ssf/day12demo1/controller/DemoController.java
@@ -1,19 +1,26 @@
 package sg.nus.iss.visa.ssf.day12demo1.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// create a controller class
+
+
+// accessing api portion of the uri
+// e.g localhost:8080/api
 @RestController
 @RequestMapping(path = "/api")
 public class DemoController {
 
+    // accessing api method 'home' to be called on uri
+    // e.g localhost:8080/api/home
     @GetMapping("/home/{name}")
     public HelloWorld getHome(@PathVariable String name){
-
-
-        //return "Hello world";
+        // return a HelloWord object from the HelloWorld.java class as json format
+        // in order to return a json format, return an object
+        // if we return a String "hello world", we will get a text not in json
+        // can use a get method to return a single var, in text and not in json
         return new HelloWorld(name);
     }
     
